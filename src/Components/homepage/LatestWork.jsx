8a556f1f1f6c3efc6c780ce02e6f0a342a1844d2
Ali <ialimalik99@gmail.com>
2024-02-26 15:41:12 -0500

    add utils folder, latest work cards

--- src/Components/homepage/LatestWork.jsx
+++ src/Components/homepage/LatestWork.jsx
@@ -1,47 +1,22 @@
 import React from "react";
+import BlogPostCard from "./Card/Card";
+import { blogPosts } from "../../util/blogposts";
 
 const LatestWork = () => {
   return (
     <section>
-      <div className="w-min md:w-1/2  md:p-10">
+      <div className="w-min md:w-1/2 md:p-4">
         <h2 className="mb-2 mt-2 p-4 md:p-10 text-2xl font-extrabold leading-loose tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-black">
           Latest Work
         </h2>
       </div>
 
-      <div className="w-full md:w-1/2 md:p-10">
-      <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        <a href="#">
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                Prototype Pattern
-          </h5>
-        </a>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-          Here are the biggest enterprise technology acquisitions of 2021 so
-          far, in reverse chronological order.
-        </p>
-        <a
-          href="#"
-          className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        >
-          Read more
-          <svg
-            className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 14 10"
-          >
-            <path
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M1 5h12m0 0L9 1m4 4L9 9"
-            />
-          </svg>
-        </a>
-      </div>
+      <div className="w-min md:w-1/2 md:p-4 flex flex-wrap">
+        {blogPosts.map((post, index) => (
+          <div key={index} className="w-full md:w-1/3 p-4 flex mb-2 mt-2">
+            <BlogPostCard title={post.title} content={post.content} />
+          </div>
+        ))}
       </div>
     </section>
   );
---
+++ src/util/blogposts.js
@@ -0,0 +1,21 @@
+export const blogPosts = [{
+        'title': 'First Post',
+        'content': 'Content goes here',
+
+    },
+    {
+        'title': 'Second Post',
+        'content': 'Content goes here. This one has more content than the previous one',
+
+    },
+    {
+        'title': 'Third Post',
+        'content': 'Content goes here',
+
+    },
+    {
+        'title': 'Fourth Post',
+        'content': 'Content goes here',
+
+    }
+]
