74a680bbcf94997979a77608bdb63634ec05fd5a
Ali <ialimalik99@gmail.com>
2024-03-11 12:25:49 -0400

    make mobile hamburger meny responsive

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -10,35 +10,24 @@ const Header = () => {
 
   return (
     <nav className="bg-gray-800 p-4">
-      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
-        {/* Navigation Links */}
-        {/* Collapsible Button (Visible on mobile only) */}
+      <div className="container mx-auto flex flex-col md:flex-row justify-between">
         {console.log(isMenuOpen)}
         <button
-          className={`block md:hidden focus:outline-none focus:ring-2 focus:ring-white focus:ring-opacity-50 ${
-            isMenuOpen ? "bg-gray-700" : ""
-          }`}
+          className={"block md:hidden focus:outline-none"}
           onClick={toggleMenu}
         >
-          <svg
-            className="h-6 w-6 fill-current text-white"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fillRule="evenodd"
-              d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zM4 9h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
-              clipRule="evenodd"
-            />
+          <svg viewBox="0 0 100 80" width="40" height="40" className="mb-2">
+            <rect width="100" height="20"></rect>
+            <rect y="30" width="100" height="20"></rect>
+            <rect y="60" width="100" height="20"></rect>
           </svg>
         </button>
 
         {/* Navigation Links (Collapsible) */}
         <ul
           className={`md:relative w-full md:static md:flex md:flex-wrap md:flex-nowrap space-x-4 md:space-x-8 text-white transition duration-300 ease-in-out 
-          ${
-            isMenuOpen ? "block bg-gray-800" : "hidden"
-          }`}
+        ${isMenuOpen ? "block bg-gray-800" : "hidden"}`}
+          style={{ alignItems: "center" }}
         >
           <li>
             <Link to="/">Home</Link>
@@ -51,7 +40,7 @@ const Header = () => {
           </li>
         </ul>
 
-        <div className="text-white text-xl font-semibold mt-4 md:mt-0 md:ml-auto"></div>
+        {/* <div className="text-white text-xl font-semibold mt-4 md:mt-0 md:ml-auto"></div> */}
       </div>
     </nav>
   );
