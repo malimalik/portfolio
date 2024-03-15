b237ccbf62c925103c9183989dfefbb804e9370e
Ali <ialimalik99@gmail.com>
2024-03-15 11:48:00 -0400

    update copy

--- src/Components/Work/workExperience.jsx
+++ src/Components/Work/workExperience.jsx
@@ -29,8 +29,9 @@ export const workExperiences = [
         <br />
 
         <p>
-          I wore multiple hats and did a wide range of work, including creating course content, visuals for courses, and of course 
-          coding features for the website. 
+          I wore multiple hats and did a wide range of work, including creating
+          course content, visuals for courses, and of course coding features for
+          the website.
         </p>
         <br />
 
@@ -52,18 +53,19 @@ export const workExperiences = [
           on the backend. To implement this feature into the existing codebase,
           I was required to learn Firebase functions, Angular RxJS subjects, all
           within a week. The biggest challenge was making sure the UI updates to
-          reflect the stats of the user who is signed in, especially using RxJS.
+          reflect the stats of the user who is signed in.
         </p>
 
         <h2 className="font-bold text-xl mb-2">Course Roadmap</h2>
 
         <p>
-          The feature you see below was more heavy on the front-end. What you
-          see below serves as a roadmap to help students the order in which to
-          take courses in. To create this graph-like structure, I used an
-          external library and the progress bar is again, specific to the user
-          signed in. The hardest part of this was making this page responsive.
-          If you visit the {" "}
+          The feature you see below was more heavy on the front-end. It serves
+          as a roadmap and helps students determine the order in which courses
+          are recommended to be completed. To create this graph-like structure,
+          I used an external library and the progress bar is specific to the
+          user signed in. The hardest part of this was making this page
+          responsive due to the external library being a little finnicky. If you
+          visit the{" "}
           <a
             href="https://www.neetcode.io/roadmap"
             className="hover:underline font-medium "
@@ -71,13 +73,14 @@ export const workExperiences = [
             roadmap page
           </a>
           , you can drag around the cards and rearrange them to your desired
-          order.
+          order, which I think is really useful.
         </p>
         <br />
         <img src={nc} alt="nc-roadmap" className="border rounded-lg" />
         <br />
         <blockquote className="p-[15px] bg-slate-100 border rounded-[5px]">
-          No progress bar under a course card indicates that the course has not been started by the user.
+          No progress bar under a course card indicates that the course has not
+          been started by the user.
         </blockquote>
       </div>
     ),
@@ -93,7 +96,7 @@ export const workExperiences = [
       <div className="mx-[30px]">
         <p className="font-sans">
           IT Innovation Lab is a department within York University. I was
-          involved in two major projects, which were launched and helped
+          involved in two major projects, both of which were launched to help
           thousands of students.
         </p>
         <br />
@@ -104,35 +107,38 @@ export const workExperiences = [
           Watson.
         </p>
         <p className="mb-2">
-          Typically, the content creators would produce the content and it would
-          be uploaded by the AI Architect. This system eliminated this and
-          significantly sped up the training process of the Virtual Assistant.
+          Typically, the content creators produce the content, which is then
+          uploaded to Watson by the AI Architect. This system enables the
+          content creators to add content, send it for approval, and then have
+          it uploaded directly to the Cloud, significantly speeding up the
+          training process of the Virtual Assistant.
         </p>
         <img src={sva} alt="stats" className="border rounded-lg" />
         <br />
         <p className="mb-2">
-          The above screenshot shows the dashboard of the Content Management
-          System and what the user sees when they log in. They are able to see
-          the latest tasks assigned to them and can also request content from
-          others in the system.
+          The above screenshot depicts the dashboard of the Content Management
+          System and presents what users see upon logging in. They can view the
+          latest tasks assigned to them and also request content from others
+          within the system.
         </p>
 
         <blockquote className="p-[15px] bg-slate-100 border rounded-[5px] mb-2 shadow-lg font-semibold">
-          The tasks section you see on the right was coded solely by me.
+          The tasks section that you see on the right was coded solely by me.{" "}
         </blockquote>
         <p className="mb-2">
-          The second feature I contributed to in this project was a complex form
-          which involved entering details of the content, form validation,
-          wiring it up with the back-end, providing user feedback etc.
+          The second feature I contributed to in this project was a complex
+          form. It involved entering details of the content, implementing form
+          validation, wiring it up with the back-end, and providing user
+          feedback.
         </p>
 
         <details>
           <summary className="font-bold mb-2">
-            Click here for a deeper dive into the feature.
+            Click here for a deeper dive into this feature.
           </summary>
           <p>
             The images below highlight the workflow of adding a new collection
-            (one type of content) and sending it to the backend cloud.
+            (one type of content).
           </p>
           <br />
           <img src={step1} alt="step-1" className="border rounded-lg mb-2" />
@@ -150,8 +156,9 @@ export const workExperiences = [
         <p className="mb-2">
           This application was created to help reduce the time it takes for the
           registrar to approve the courses that faculties wish to offer during
-          an academic year. Before this project, all the details would be
-          communicated via email and Excel sheets, which was highly ineffective.
+          an academic year. Prior to this project, all the details would be
+          communicated via email and Excel sheets, which proved to be highly
+          ineffective.
         </p>
 
         <img
@@ -166,27 +173,26 @@ export const workExperiences = [
         />
 
         <p className="mb-2">
-          The table you see above was the bread and butter of the project. I
-          solely coded this table and most of the complexity lies in the backend
-          of it.
+          The table you see above was the backbone of the project. I solely
+          coded this table, and most of its complexity lies in the backend.
         </p>
 
         <p className="mb-2">
           All the information above is fetched from a master database, which is
           updated once a week to reflect the latest changes through a CRON job.
-          Fetching the data from the database, getting it in the right format
-          and mapping it correctly was the biggest challenge, along with the
-          multiple filters that are observed above.
+          The biggest challenge was fetching the data from the database, getting
+          it in the right format, and mapping it correctly, along with
+          implementing the multiple filters observed above.
         </p>
         <p className="mb-2">
           The second notable feature I worked on was a version history feature.
-          Because there are multiple users of the system, multiple users can
-          make changes to the details of the course using the "Actions" option.
+          Since there are multiple users of the system, each user can make
+          changes to the details of the course using the 'Actions' option.
         </p>
 
         <p className="mb-2">
-          The highlighted field depicts a change in the value of a field,
-          allowing the user to view the changes made. This made it so that
+          The highlighted field indicates a change in the value of a field,
+          enabling the user to view the modifications made. This ensured that
           everyone was on the same page.
         </p>
         <img
@@ -203,16 +209,17 @@ export const workExperiences = [
     about:
       "My first ever development job, building a digital badge plug-in to automate course completion progress.",
 
-    modalHeader: "My Work at Faculty of Health @ York University",
+    modalHeader: "My Work at Faculty of Health - York University",
     body: (
       <div className="mx-[30px]">
         <h2 className="font-bold text-xl mb-2">Custom Theme</h2>
         <p className="font-sans">
-          At Faculty of Health, I was required to create a custom theme within
-          the University's Learning Management System, designed specifically for
-          the students enrolling in courses in the Faculty. I delivered this
-          within the first month of being on-boarded on the job. The theme was
-          mostly done with CSS, PHP, and Vanilla JavaScript.
+          At the Faculty of Health, I was tasked with creating a custom theme
+          within the University's Learning Management System, tailored
+          specifically for students enrolling in courses within the Faculty. I
+          successfully delivered this within the first month of starting the
+          job. The theme was primarily developed using CSS, PHP, and Vanilla
+          JavaScript.
         </p>
         <br />
         <br />
@@ -232,11 +239,11 @@ export const workExperiences = [
           Management System handling it all.
         </p>
         <p>
-          This feature required me to integrate an external API through a badge
-          awarder named Credly, design and create database tables to store
-          details of the badge and wire up the front-end with the back-end. The
-          most challenging part was collecting the data, and sending it in a
-          format that the back-end expects.
+          Upon completion of a course and all assignments, students are offered
+          certificates of completion. Previously, this process was manual,
+          involving the manual distribution of badges via email. With the
+          implementation of this plugin, badges are now awarded automatically,
+          with the Learning Management System handling the entire process.
         </p>
 
         <br />
@@ -249,7 +256,7 @@ export const workExperiences = [
         <br />
         <blockquote className="p-[15px] bg-slate-100 border rounded-[5px]">
           Fun fact: This screenshot was actually taken the first time I got the
-          front-end and the back-end to work.
+          front-end and the back-end to work together.
         </blockquote>
       </div>
     ),
